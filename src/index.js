9ef6c9165096a78708d81a47f5dd1c46389f4c7a
Dominik <dominikant99@gmail.com>
2023-11-14 14:46:47 +0100

    Update and improve error handling of weather api call

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import "./style.css";
 import { callWeatherApi } from "./weather-api";
 
-callWeatherApi();
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+async function apiCall() {
+  try {
+    const data = await callWeatherApi(searchInput.value);
+    console.log(data);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
+
+searchButton.addEventListener("click", apiCall);
--- src/weather-api.js
+++ src/weather-api.js
@@ -1,14 +1,20 @@
 const apiKey = "b067ed5c72424299acc131107230311";
-const url = "http://api.weatherapi.com/v1"
-const apiMethod = "/current.json"
-const parameter = "Paris"
+const url = "http://api.weatherapi.com/v1";
+const apiMethod = "/current.json";
 
-async function callWeatherApi() {
-    const response = await fetch(`${url}${apiMethod}?key=${apiKey}&q=${parameter}`);
+async function callWeatherApi(parameter) {
+  try {
+    const response = await fetch(
+      `${url}${apiMethod}?key=${apiKey}&q=${parameter}`,
+    );
+    if (!response.error)
+      throw new Error("You entered city that does not exist");
+
     const responseData = await response.json();
-
-
+    return responseData;
+  } catch (error) {
+    console.log(`${error}`);
+  }
 }
 
-
-export {callWeatherApi};
+export { callWeatherApi };
